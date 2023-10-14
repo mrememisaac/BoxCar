bd631984cc5fb3d35fb69b75bcca1939801cddff
Emem Isaac <me@ememisaac.com>
2023-10-14 22:02:25 +0100

    tests get option pack

---
+++ BoxCar.Admin.Tests/OptionPacksTests/GetOptionPackTests.cs
@@ -0,0 +1,29 @@
+﻿using BoxCar.Admin.Core.Features.OptionPacks.GetOptionPack;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Shouldly;
+
+namespace BoxCar.Admin.Tests.OptionPackTests
+{
+
+    public class GetOptionPackTests : OptionPackTestsBase
+    {
+
+        [Fact]
+        public async void Can_Get_OptionPack_By_Id()
+        {
+            var collection = await repository.GetAllAsync(cancellationToken);
+            var factoryId = collection.Last().Id;
+
+            var query = new GetOptionPackByIdQuery { Id = factoryId };
+            var logger = new Mock<ILogger<GetOptionPackByIdQueryHandler>>();
+            var validator = new GetOptionPackByIdQueryValidator();
+            var handler = new GetOptionPackByIdQueryHandler(repository, logger.Object, validator, mapper);
+
+
+            var factory = await handler.Handle(query, cancellationToken);
+
+            factory.ShouldNotBeNull();
+        }
+    }
+}
