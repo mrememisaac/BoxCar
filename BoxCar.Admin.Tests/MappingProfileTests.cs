1d4923baeec01c6328d4fb2d3595de6aee2402f1
Emem Isaac <me@ememisaac.com>
2023-10-14 19:53:50 +0100

    mapping tests

---
+++ BoxCar.Admin.Tests/MappingProfileTests.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+using BoxCar.Admin.Tests.FactoryTests;
+
+public class MappingProfileTests : FactoryTestsBase
+{
+    [Fact]
+    public void ValidateMappingConfigurationTest()
+    {
+        mapper.ConfigurationProvider.AssertConfigurationIsValid();
+    }
+}
