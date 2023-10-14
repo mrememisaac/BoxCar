129326fa4557da3a06994a5a5a04489525a93057
Emem Isaac <me@ememisaac.com>
2023-10-14 17:19:37 +0100

    Adds Engine add/get endpoints

---
+++ BoxCar.Admin.Api/Controllers/EnginesController.cs
@@ -0,0 +1,50 @@
+using Admin.Core.Features.Engines.AddEngine;
+using Admin.Core.Features.Engines.GetEngine;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoxCar.Admin.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EnginesController : ControllerBase
+    {
+        private readonly ILogger<EnginesController> _logger;
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public EnginesController(ILogger<EnginesController> logger, IMediator mediator, IMapper mapper)
+        {
+            _logger = logger;
+            _mediator = mediator;
+            this._mapper = mapper;
+        }
+
+        [HttpPost("AddEngine", Name = nameof(AddEngine))]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<AddEngineResponse>> AddEngine(AddEngineDto request)
+        {
+            var response = await _mediator.Send(_mapper.Map<AddEngineCommand>(request));
+            return CreatedAtAction(nameof(GetEngineById), response.Value);
+        }
+
+        [HttpGet("GetEngineById", Name = nameof(GetEngineById))]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<GetEngineByIdResponse>> GetEngineById(Guid id)
+        {
+            var response = await _mediator.Send(new GetEngineByIdQuery { Id = id });
+            if (response == null)
+            {
+                return NotFound();
+            }
+            return Ok(response);
+        }
+    }
+}
