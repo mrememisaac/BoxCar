7fa4a19b277c0dc2b384262676a4a263c0ef1ba9
Emem Isaac <me@ememisaac.com>
2023-10-14 14:24:38 +0100

    Adds persistence contract

---
+++ Admin.Core/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,24 @@
+﻿using BoxCar.Admin.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.Core.Contracts.Persistence
+{
+    public interface IAsyncRepository<T, TId> where T : BaseEntity<TId>
+    {
+        Task<T> CreateAsync(T entity);
+
+        Task UpdateAsync(T entity);
+
+        Task DeleteAsync(T entity);
+
+        Task<T?> GetByIdAsync(TId id);
+
+        Task<IReadOnlyList<T>> GetAllAsync();
+
+        Task<IReadOnlyList<T>> GetPagedAsync(int page, int pageSize);
+    }
+}
