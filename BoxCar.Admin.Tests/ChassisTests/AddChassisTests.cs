6acdc2f65bfcc2ae99ddd03d62a12c3291985fa4
Emem Isaac <me@ememisaac.com>
2023-10-14 21:55:56 +0100

    Tests for Chassis Add

---
+++ BoxCar.Admin.Tests/ChassisTests/AddChassisTests.cs
@@ -0,0 +1,32 @@
+﻿using BoxCar.Admin.Core.Features.Chasis.AddChassis;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Shouldly;
+
+namespace BoxCar.Admin.Tests.ChassisTests
+{
+    public class AddChassisTests : ChassisTestsBase
+    {
+
+        [Fact]
+        public async void Can_Add_Chassis()
+        {
+            var command = new AddChassisCommand
+            {
+                Id = Guid.NewGuid(),
+                Name = "East Chassis",
+                Description = "East Chassis is a great chassis designed in the East"
+            };
+            var logger = new Mock<ILogger<AddChassisCommandHandler>>();
+            var validator = new AddChassisCommandValidator();
+            var handler = new AddChassisCommandHandler(mapper, repository, logger.Object, validator);
+
+            var collection = await repository.GetAllAsync(cancellationToken);
+            var count = collection.Count();
+
+            await handler.Handle(command, cancellationToken);
+
+            collection.Count.ShouldBe(count + 1);
+        }
+    }
+}
---
+++ BoxCar.Admin.Tests/ChassisTests/ChassisTestsBase.cs
@@ -0,0 +1,21 @@
+﻿using BoxCar.Admin.Core.Contracts.Persistence;
+using AutoMapper;
+using BoxCar.Admin.Domain;
+using Microsoft.Extensions.DependencyInjection;
+using BoxCar.Admin.Core.Profiles;
+using BoxCar.Admin.Core;
+using BoxCar.Admin.Tests.Fakes.Repositories;
+
+namespace BoxCar.Admin.Tests.ChassisTests
+{
+    public class ChassisTestsBase : EntityTestsBase<Chassis>
+    {
+        protected readonly IAsyncRepository<Chassis, Guid> repository;
+
+        public ChassisTestsBase()
+        {
+            repository = _serviceProvider.GetService<ListBasedChassisRepository>() ?? new ListBasedChassisRepository();
+        }
+
+    }
+}
