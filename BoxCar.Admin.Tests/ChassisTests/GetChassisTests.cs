3de0138b28fb22dc541047bfa602c1a6c42e5925
Emem Isaac <me@ememisaac.com>
2023-10-14 21:56:11 +0100

    Tests for Chassis get

---
+++ BoxCar.Admin.Tests/ChassisTests/GetChassisTests.cs
@@ -0,0 +1,29 @@
+﻿using BoxCar.Admin.Core.Features.Chasis.GetChassis;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Shouldly;
+
+namespace BoxCar.Admin.Tests.ChassisTests
+{
+
+    public class GetChassisTests : ChassisTestsBase
+    {
+
+        [Fact]
+        public async void Can_Get_Chassis_By_Id()
+        {
+            var collection = await repository.GetAllAsync(cancellationToken);
+            var factoryId = collection.Last().Id;
+
+            var query = new GetChassisByIdQuery { Id = factoryId };
+            var logger = new Mock<ILogger<GetChassisByIdQueryHandler>>();
+            var validator = new GetChassisByIdQueryValidator();
+            var handler = new GetChassisByIdQueryHandler(repository, logger.Object, validator, mapper);
+
+
+            var factory = await handler.Handle(query, cancellationToken);
+
+            factory.ShouldNotBeNull();
+        }
+    }
+}
