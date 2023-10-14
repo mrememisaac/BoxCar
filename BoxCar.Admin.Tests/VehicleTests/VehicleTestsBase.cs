d70132900339b503280cf3725d85a434a1f0d8af
Emem Isaac <me@ememisaac.com>
2023-10-14 22:02:37 +0100

    foundation for vehicle tests

---
+++ BoxCar.Admin.Tests/VehicleTests/VehicleTestsBase.cs
@@ -0,0 +1,26 @@
+﻿using BoxCar.Admin.Core.Contracts.Persistence;
+using AutoMapper;
+using BoxCar.Admin.Domain;
+using Microsoft.Extensions.DependencyInjection;
+using BoxCar.Admin.Core.Profiles;
+using BoxCar.Admin.Core;
+using BoxCar.Admin.Tests.Fakes.Repositories;
+
+namespace BoxCar.Admin.Tests.VehicleTests
+{
+    public class VehicleTestsBase : EntityTestsBase<Vehicle>
+    {
+        protected readonly IAsyncRepository<Vehicle, Guid> repository;
+        protected readonly IAsyncRepository<Chassis, Guid> chassisRepository;
+        protected readonly IAsyncRepository<Engine, Guid> enginesRepository;
+        protected readonly IAsyncRepository<OptionPack, Guid> optionPacksRepository;
+
+        public VehicleTestsBase()
+        {
+            repository = new ListBasedVehicleRepository();
+            chassisRepository = new ListBasedChassisRepository();
+            enginesRepository = new ListBasedEngineRepository();
+            optionPacksRepository = new ListBasedOptionPackRepository();
+        }
+    }
+}
