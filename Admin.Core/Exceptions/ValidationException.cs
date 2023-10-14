da0876025c83cc178a20d0cb61dd4291473151ee
Emem Isaac <me@ememisaac.com>
2023-10-14 14:24:50 +0100

    Adds custom exceptions

---
+++ Admin.Core/Exceptions/ValidationException.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation.Results;
+
+namespace Admin.Core.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public List<string> Errors { get; set; }
+
+        public ValidationException(ValidationResult validationResult)
+        {
+            Errors = new List<string>();
+            foreach (var error in validationResult.Errors)
+            {
+                Errors.Add(error.ErrorMessage);
+            }
+        }
+    }
+
+}
