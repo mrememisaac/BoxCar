4d729d3da3ecbff5e5bce3b5117512b8139fe9ac
Emem Isaac <me@ememisaac.com>
2023-10-14 22:03:24 +0100

    tests warehouse get

---
+++ BoxCar.Admin.Tests/WarehouseTests/GetWareHouseTests.cs
@@ -0,0 +1,29 @@
+﻿using BoxCar.Admin.Core.Features.Warehouses.GetWareHouse;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Shouldly;
+
+namespace BoxCar.Admin.Tests.WareHouseTests
+{
+
+    public class GetWareHouseTests : WareHouseTestsBase
+    {
+
+        [Fact]
+        public async void Can_Get_WareHouse_By_Id()
+        {
+            var collection = await repository.GetAllAsync(cancellationToken);
+            var factoryId = collection.Last().Id;
+
+            var query = new GetWareHouseByIdQuery { Id = factoryId };
+            var logger = new Mock<ILogger<GetWareHouseByIdQueryHandler>>();
+            var validator = new GetWareHouseByIdQueryValidator();
+            var handler = new GetWareHouseByIdQueryHandler(repository, logger.Object, validator, mapper);
+
+
+            var factory = await handler.Handle(query, cancellationToken);
+
+            factory.ShouldNotBeNull();
+        }
+    }
+}
