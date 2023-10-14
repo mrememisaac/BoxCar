0cb68a8145ef7d8af2b07d7428372319ea1660ca
Emem Isaac <me@ememisaac.com>
2023-10-14 22:03:06 +0100

    foundation for warehouse tests

---
+++ BoxCar.Admin.Tests/WarehouseTests/WareHouseTestsBase.cs
@@ -0,0 +1,20 @@
+﻿using BoxCar.Admin.Core.Contracts.Persistence;
+using AutoMapper;
+using BoxCar.Admin.Domain;
+using Microsoft.Extensions.DependencyInjection;
+using BoxCar.Admin.Core.Profiles;
+using BoxCar.Admin.Core;
+using BoxCar.Admin.Tests.Fakes.Repositories;
+
+namespace BoxCar.Admin.Tests.WareHouseTests
+{
+    public class WareHouseTestsBase : EntityTestsBase<WareHouse>
+    {
+        protected readonly IAsyncRepository<WareHouse, Guid> repository;
+
+        public WareHouseTestsBase()
+        {
+            repository = new ListBasedWareHouseRepository();
+        }
+    }
+}
