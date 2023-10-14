2bc7efd909cfc8fca387c4e8b0687c6ab00e5a83
Emem Isaac <me@ememisaac.com>
2023-10-14 14:34:02 +0100

    Adds Address feature

---
+++ Admin.Core/Features/AddressDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Admin.Core.Features
+{
+    public class AddressDto
+    {
+        public string Street { get; private set; }
+        public string City { get; private set; }
+        public string State { get; private set; }
+        public string PostalCode { get; private set; }
+        public string Country { get; private set; }
+
+    }
+}
---
+++ Admin.Core/Features/AddressDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using Admin.Core.Features.Factories.AddFactory;
+using FluentValidation;
+
+namespace Admin.Core.Features
+{
+    public class AddressDtoValidator : AbstractValidator<AddressDto>
+    {
+        public AddressDtoValidator()
+        {
+            RuleFor(p => p.Street).NotEmpty().MaximumLength(50);
+            RuleFor(p => p.City).NotEmpty().MaximumLength(50);
+            RuleFor(p => p.PostalCode).NotEmpty().MaximumLength(10);
+            RuleFor(p => p.State).NotEmpty().MaximumLength(50);
+            RuleFor(p => p.Country).NotEmpty().MaximumLength(50);
+        }
+    }
+}
