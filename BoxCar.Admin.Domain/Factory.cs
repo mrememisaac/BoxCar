3d7b28652309ba46ce0aba7d7ac70b7fffe7c81d
Emem Isaac <me@ememisaac.com>
2023-10-14 14:42:29 +0100

    Adds new entities

---
+++ BoxCar.Admin.Domain/Factory.cs
@@ -0,0 +1,15 @@
+﻿namespace BoxCar.Admin.Domain
+{
+    public class Factory : BaseEntity<Guid>
+    {
+        public string Name { get; set; }
+
+        public Address Address { get; set; }
+
+        public Factory(string name, Address address)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Address = address ?? throw new ArgumentNullException(nameof(address));
+        }
+    }
+}
---
+++ BoxCar.Admin.Domain/WareHouse.cs
@@ -0,0 +1,15 @@
+﻿namespace BoxCar.Admin.Domain
+{
+    public class WareHouse : BaseEntity<Guid>
+    {
+        public string Name { get; private set; }
+
+        public Address Address { get; set; }
+
+        public WareHouse(string name, Address address)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Address = address ?? throw new ArgumentNullException(nameof(address));
+        }
+    }
+}
