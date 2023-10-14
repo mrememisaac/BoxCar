eb287b0307ef247fab4ef428eb955a4870252356
Emem Isaac <me@ememisaac.com>
2023-10-14 21:56:43 +0100

    Test for engine get

---
+++ BoxCar.Admin.Tests/EngineTests/GetEngineTests.cs
@@ -0,0 +1,29 @@
+﻿using BoxCar.Admin.Core.Features.Engines.GetEngine;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Shouldly;
+
+namespace BoxCar.Admin.Tests.EngineTests
+{
+
+    public class GetEngineTests : EngineTestsBase
+    {
+
+        [Fact]
+        public async void Can_Get_Engine_By_Id()
+        {
+            var collection = await repository.GetAllAsync(cancellationToken);
+            var factoryId = collection.Last().Id;
+
+            var query = new GetEngineByIdQuery { Id = factoryId };
+            var logger = new Mock<ILogger<GetEngineByIdQueryHandler>>();
+            var validator = new GetEngineByIdQueryValidator();
+            var handler = new GetEngineByIdQueryHandler(repository, logger.Object, validator, mapper);
+
+
+            var factory = await handler.Handle(query, cancellationToken);
+
+            factory.ShouldNotBeNull();
+        }
+    }
+}
