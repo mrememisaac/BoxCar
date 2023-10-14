79f547bffdc84f8b004dafe8c5cf80b66685af71
Emem Isaac <me@ememisaac.com>
2023-10-14 22:01:55 +0100

    adds options pack test base

---
+++ BoxCar.Admin.Tests/OptionPacksTests/OptionPackTestsBase.cs
@@ -0,0 +1,20 @@
+﻿using BoxCar.Admin.Core.Contracts.Persistence;
+using AutoMapper;
+using BoxCar.Admin.Domain;
+using Microsoft.Extensions.DependencyInjection;
+using BoxCar.Admin.Core.Profiles;
+using BoxCar.Admin.Core;
+using BoxCar.Admin.Tests.Fakes.Repositories;
+
+namespace BoxCar.Admin.Tests.OptionPackTests
+{
+    public class OptionPackTestsBase : EntityTestsBase<OptionPack>
+    {
+        protected readonly IAsyncRepository<OptionPack, Guid> repository;
+
+        public OptionPackTestsBase()
+        {
+            repository = new ListBasedOptionPackRepository();
+        }
+    }
+}
