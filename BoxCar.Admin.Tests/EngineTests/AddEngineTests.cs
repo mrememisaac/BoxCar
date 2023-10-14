3ac54abc1308a340386795f7e564ff1af70b4a57
Emem Isaac <me@ememisaac.com>
2023-10-14 21:56:32 +0100

    Tests for Engine add

---
+++ BoxCar.Admin.Tests/EngineTests/AddEngineTests.cs
@@ -0,0 +1,34 @@
+﻿using BoxCar.Admin.Core.Features.Engines.AddEngine;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Shouldly;
+
+namespace BoxCar.Admin.Tests.EngineTests
+{
+    public class AddEngineTests : EngineTestsBase
+    {
+
+        [Fact]
+        public async void Can_Add_Engine()
+        {
+            var command = new AddEngineCommand
+            {
+                Id = Guid.NewGuid(),
+                Name = "East Engine",
+                FuelType = Domain.FuelType.Gasoline,
+                IgnitionMethod = Domain.IgnitionMethod.HCCI,
+                Strokes = 4
+            };
+            var logger = new Mock<ILogger<AddEngineCommandHandler>>();
+            var validator = new AddEngineCommandValidator();
+            var handler = new AddEngineCommandHandler(mapper, repository, logger.Object, validator);
+
+            var collection = await repository.GetAllAsync(cancellationToken);
+            var count = collection.Count();
+
+            await handler.Handle(command, cancellationToken);
+
+            collection.Count.ShouldBe(count + 1);
+        }
+    }
+}
---
+++ BoxCar.Admin.Tests/EngineTests/EngineTestsBase.cs
@@ -0,0 +1,21 @@
+﻿using BoxCar.Admin.Core.Contracts.Persistence;
+using AutoMapper;
+using BoxCar.Admin.Domain;
+using Microsoft.Extensions.DependencyInjection;
+using BoxCar.Admin.Core.Profiles;
+using BoxCar.Admin.Core;
+using BoxCar.Admin.Tests.Fakes.Repositories;
+
+namespace BoxCar.Admin.Tests.EngineTests
+{
+    public class EngineTestsBase : EntityTestsBase<Engine>
+    {
+        protected readonly IAsyncRepository<Engine, Guid> repository;
+
+        public EngineTestsBase()
+        {
+            repository = new ListBasedEngineRepository();
+        }
+
+    }
+}
