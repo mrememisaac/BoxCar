7c17d1ca810cf3e0401d793de5dcd5bbf8c87af3
Emem Isaac <me@ememisaac.com>
2023-10-14 14:34:20 +0100

    Adds a standard return type

---
+++ Admin.Core/Features/Result.cs
@@ -0,0 +1,25 @@
+﻿namespace Admin.Core.Features
+{
+    public class Result<T> where T : class
+    {
+        public bool Success { get; set; }
+
+        public string Message { get; set; } = string.Empty;
+
+        public Result()
+        {
+            Success = true;
+        }
+
+        public Result(bool success)
+        {
+            Success = false;
+        }
+        public Result(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+
+    }
+}
