fd38e79a98517b17be7374429d7b1388a997f80a
Emem Isaac <me@ememisaac.com>
2023-10-14 19:51:17 +0100

    New mapping profiles

---
+++ Admin.Core/Profiles/AddressProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BoxCar.Admin.Domain;
+using BoxCar.Admin.Core.Features;
+
+namespace BoxCar.Admin.Core.Profiles
+{
+    public class AddressProfiles : Profile
+    {
+        public AddressProfiles()
+        {
+            CreateMap<AddressDto, Address>()
+                .ConstructUsing(address => new Address(address.Street, address.City, address.State, address.PostalCode, address.Country))
+                .ReverseMap();
+        }
+    }
+}
---
+++ Admin.Core/Profiles/OptionProfiles.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using BoxCar.Admin.Domain;
+using BoxCar.Admin.Core.Features.OptionPacks.AddOptionPack;
+using BoxCar.Admin.Core.Features.Vehicles.GetVehicle;
+
+namespace BoxCar.Admin.Core.Profiles
+{
+    public class OptionProfiles : Profile
+    {
+        public OptionProfiles()
+        {
+            CreateMap<OptionDto, Option>()
+                .ForMember(d => d.CreatedBy, opt => opt.Ignore())
+                .ForMember(d => d.CreatedDate, opt => opt.Ignore())
+                .ForMember(d => d.UpdatedBy, opt => opt.Ignore())
+                .ForMember(d => d.UpdatedDate, opt => opt.Ignore())
+                .ReverseMap();
+            CreateMap<AddOptionDto, Option>()
+                .ForMember(d => d.CreatedBy, opt => opt.Ignore())
+                .ForMember(d => d.CreatedDate, opt => opt.Ignore())
+                .ForMember(d => d.UpdatedBy, opt => opt.Ignore())
+                .ForMember(d => d.UpdatedDate, opt => opt.Ignore())
+                .ReverseMap();
+        }
+    }
+}
