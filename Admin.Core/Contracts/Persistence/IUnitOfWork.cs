1336dfeaf8c2ed6f6ab0ec27ac5bfa5cbce83dc8
Emem Isaac <me@ememisaac.com>
2023-10-15 07:40:33 +0100

    Added to facilitate atomic transactions wrapping inserts to entity and eventlog trail

---
+++ Admin.Core/Contracts/Persistence/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using BoxCar.Admin.Domain;
+
+namespace BoxCar.Admin.Core.Contracts.Persistence
+{
+    public interface IUnitOfWork
+    {
+        IAsyncRepository<Vehicle, Guid> VehicleRepository { get; }
+        IAsyncRepository<Engine, Guid> EngineRepository { get; }
+        IAsyncRepository<Factory, Guid> FactoryRepository { get; }
+        IAsyncRepository<WareHouse, Guid> WareHouseRepository { get; }
+        IAsyncRepository<Chassis, Guid> ChassisRepository { get; }
+        IAsyncRepository<OptionPack, Guid> OptionPackRepository { get; }
+        IAsyncRepository<IntegrationEvent, Guid> IntegrationEventRepository { get; }
+
+        void Commit();
+        void Rollback();
+    }
+}
---
+++ BoxCar.Admin.Persistence/UnitOfWork.cs
@@ -0,0 +1,68 @@
+﻿using BoxCar.Admin.Core.Contracts.Persistence;
+using BoxCar.Admin.Domain;
+
+namespace BoxCar.Admin.Persistence
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly BoxCarDbContext _dbContext;
+        private IAsyncRepository<Vehicle, Guid> _vehicleRepository;
+        private IAsyncRepository<Chassis, Guid> _chassisRepository;
+        private IAsyncRepository<Engine, Guid> _engineRepository;
+        private IAsyncRepository<OptionPack, Guid> _optionPackRepository;
+        private IAsyncRepository<Factory, Guid> _factoryRepository;
+        private IAsyncRepository<WareHouse, Guid> _warehouseRepository;
+        private IAsyncRepository<IntegrationEvent, Guid> _integrationEventRepository;
+
+        public UnitOfWork(BoxCarDbContext databaseContext)
+        {
+            _dbContext = databaseContext;
+        }
+
+        public IAsyncRepository<Vehicle, Guid> VehicleRepository
+        {
+            get { return _vehicleRepository = _vehicleRepository ?? new BaseRepository<Vehicle, Guid>(_dbContext); }
+        }
+
+
+        public IAsyncRepository<Engine, Guid> EngineRepository
+        {
+            get { return _engineRepository = _engineRepository ?? new BaseRepository<Engine, Guid>(_dbContext); }
+        }
+
+        public IAsyncRepository<Factory, Guid> FactoryRepository
+        {
+            get { return _factoryRepository = _factoryRepository ?? new BaseRepository<Factory, Guid>(_dbContext); }
+        }
+
+        public IAsyncRepository<WareHouse, Guid> WareHouseRepository
+        {
+            get { return _warehouseRepository = _warehouseRepository ?? new BaseRepository<WareHouse, Guid>(_dbContext); }
+        }
+
+        public IAsyncRepository<Chassis, Guid> ChassisRepository
+        {
+            get { return _chassisRepository = _chassisRepository ?? new BaseRepository<Chassis, Guid>(_dbContext); }
+        }
+
+        public IAsyncRepository<OptionPack, Guid> OptionPackRepository
+        {
+            get { return _optionPackRepository = _optionPackRepository ?? new BaseRepository<OptionPack, Guid>(_dbContext); }
+        }
+
+        public IAsyncRepository<IntegrationEvent, Guid> IntegrationEventRepository
+        {
+            get { return _integrationEventRepository = _integrationEventRepository ?? new BaseRepository<IntegrationEvent, Guid>(_dbContext); }
+        }
+
+        public void Commit()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void Rollback()
+        {
+            _dbContext.Dispose();
+        }
+    }
+}
