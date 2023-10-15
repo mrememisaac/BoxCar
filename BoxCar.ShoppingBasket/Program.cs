73cbbca84507e294e252507c2a7ba797c4cfc4a7
Emem Isaac <me@ememisaac.com>
2023-10-15 22:52:35 +0100

    adds configuration

--- BoxCar.ShoppingBasket/Program.cs
+++ BoxCar.ShoppingBasket/Program.cs
@@ -1,8 +1,26 @@
+using BoxCar.ShoppingBasket.DbContexts;
+using BoxCar.ShoppingBasket.Repositories;
+using BoxCar.ShoppingBasket.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddScoped<IBasketLinesRepository, BasketLinesRepository>();
+builder.Services.AddScoped<IVehicleRepository, VehicleRepository>();
+builder.Services.AddHttpClient<IVehicleCatalogService, VehicleCatalogService>(c =>
+    c.BaseAddress = new Uri(builder.Configuration["ApiConfigs:VehicleCatalog:Uri"]));
+
+builder.Services.AddDbContext<ShoppingBasketDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
