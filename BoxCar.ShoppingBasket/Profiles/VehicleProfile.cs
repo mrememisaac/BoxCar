eb42bca43df75332b9fad3b2a67637cf76d6446b
Emem Isaac <me@ememisaac.com>
2023-10-15 22:48:57 +0100

    Adds mapping profiles

---
+++ BoxCar.ShoppingBasket/Profiles/VehicleProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace BoxCar.ShoppingBasket.Profiles
+{
+    public class VehicleProfile: Profile
+    {
+        public VehicleProfile()
+        {
+            CreateMap<Entities.Vehicle, Models.Vehicle>().ReverseMap();
+            CreateMap<Entities.Chassis, Models.Chassis>().ReverseMap();
+            CreateMap<Entities.Engine, Models.Engine>().ReverseMap();
+            CreateMap<Entities.OptionPack, Models.OptionPack>().ReverseMap();
+            CreateMap<Entities.Option, Models.Option>().ReverseMap();
+        }
+    }
+}
