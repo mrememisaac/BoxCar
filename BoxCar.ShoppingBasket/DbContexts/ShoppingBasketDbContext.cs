a71e2b0edbcabc852a5382301574fee9266a3e1d
Emem Isaac <me@ememisaac.com>
2023-10-15 22:48:44 +0100

    Adds the dbcontext

---
+++ BoxCar.ShoppingBasket/DbContexts/ShoppingBasketDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BoxCar.ShoppingBasket.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BoxCar.ShoppingBasket.DbContexts
+{
+    public class ShoppingBasketDbContext : DbContext
+    {
+        public ShoppingBasketDbContext(DbContextOptions<ShoppingBasketDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketLine> BasketLines { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+    }
+}
