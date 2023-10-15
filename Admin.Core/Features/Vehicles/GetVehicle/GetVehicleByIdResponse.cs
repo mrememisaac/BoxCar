9ef7155c55c2a2c132fb441fb402b177464660a3
Emem Isaac <me@ememisaac.com>
2023-10-15 05:40:24 +0100

    Clean up, add price

--- Admin.Core/Features/Vehicles/GetVehicle/GetVehicleByIdResponse.cs
+++ Admin.Core/Features/Vehicles/GetVehicle/GetVehicleByIdResponse.cs
@@ -1,5 +1,4 @@
 ﻿using BoxCar.Admin.Core.Features.OptionPacks.AddOptionPack;
-using BoxCar.Admin.Domain;
 
 namespace BoxCar.Admin.Core.Features.Vehicles.GetVehicle
 {
@@ -15,45 +14,8 @@ namespace BoxCar.Admin.Core.Features.Vehicles.GetVehicle
 
         public OptionPackDto OptionPack { get; set; }
 
-    }
+        public int Price { get; set; }
 
-    public class OptionPackDto
-    {
-        public Guid Id { get; set; }
-
-        public string Name { get; set; } = null!;
-
-        public List<OptionDto> Options = new List<OptionDto>();
-    }
-
-    public class OptionDto
-    {
-        public Guid Id { get; set; }
-
-        public string Name { get; set; } = null!;
-
-        public string Value { get; set; } = null!;
-    }
-
-    public class ChassisDto
-    {
-        public Guid Id { get; set; }
-
-        public string Name { get; set; } = null!;
-
-        public string Description { get; set; } = string.Empty;
-    }
-    public class EngineDto
-    {
-        public Guid Id { get; set; }
-
-        public string Name { get; set; } = null!;
-
-        public FuelType FuelType { get; set; }
-
-        public IgnitionMethod IgnitionMethod { get; set; }
-
-        public int Strokes { get; set; }
 
     }
 }
