c305c8a0756b9423a1d340f69740a112bf528879
Emem Isaac <me@ememisaac.com>
2023-10-15 22:50:47 +0100

    Adds sync messaging service with catalogue

---
+++ BoxCar.ShoppingBasket/Services/IVehicleCatalogService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using BoxCar.ShoppingBasket.Entities;
+
+namespace BoxCar.ShoppingBasket.Services
+{
+    public interface IVehicleCatalogService
+    {
+        Task<Vehicle> GetVehicle(Guid id);
+    }
+}
---
+++ BoxCar.ShoppingBasket/Services/VehicleCatalogService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BoxCar.ShoppingBasket.Entities;
+using BoxCar.ShoppingBasket.Extensions;
+
+namespace BoxCar.ShoppingBasket.Services
+{
+    public class VehicleCatalogService : IVehicleCatalogService
+    {
+        private readonly HttpClient client;
+
+        public VehicleCatalogService(HttpClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task<Vehicle> GetVehicle(Guid id)
+        {
+            var response = await client.GetAsync($"/api/vehicles/{id}");
+            return await response.ReadContentAs<Vehicle>();
+        }
+    }
+}
