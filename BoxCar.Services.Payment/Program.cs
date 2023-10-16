ecd4e65ebd9249872c9583a91c125efac5ab36b6
Emem Isaac <me@ememisaac.com>
2023-10-16 20:34:43 +0100

    configures services

--- BoxCar.Services.Payment/Program.cs
+++ BoxCar.Services.Payment/Program.cs
@@ -1,3 +1,6 @@
+using BoxCar.Integration.MessageBus;
+using BoxCar.Services.Payment.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHostedService<ServiceBusListener>();
+builder.Services.AddHttpClient<IExternalGatewayPaymentService, ExternalGatewayPaymentService>(c =>
+    c.BaseAddress = new Uri(builder.Configuration["ApiConfigs:ExternalPaymentGateway:Uri"]));
+
+builder.Services.AddSingleton<IMessageBus, AzServiceBusMessageBus>();
 
 var app = builder.Build();
 
