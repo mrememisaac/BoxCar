a7769d1da64a5ff6367785718d353e94ecc493d2
Emem Isaac <me@ememisaac.com>
2023-10-16 15:41:31 +0100

    Adds and configures the ordering service

---
+++ BoxCar.Ordering/Program.cs
@@ -0,0 +1,53 @@
+using BoxCar.Integration.MessageBus;
+using BoxCar.Ordering.DbContexts;
+using BoxCar.Ordering.Extensions;
+using BoxCar.Ordering.Messaging;
+using BoxCar.Ordering.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add builder.Services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddDbContext<OrderDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+
+//Specific DbContext for use from singleton AzServiceBusConsumer
+var optionsBuilder = new DbContextOptionsBuilder<OrderDbContext>();
+optionsBuilder.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+builder.Services.AddSingleton(new OrderRepository(optionsBuilder.Options));
+
+builder.Services.AddSingleton<IMessageBus, AzServiceBusMessageBus>();
+builder.Services.AddSingleton<IAzServiceBusConsumer, AzServiceBusConsumer>();
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseAzServiceBusConsumer();
+
+app.Run();
