e26d8ddf2773b2aa8508050e1037e6f72013e7b5
Emem Isaac <me@ememisaac.com>
2023-10-16 15:38:26 +0100

    Adds Order entities

---
+++ BoxCar.Ordering/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace BoxCar.Ordering.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public int OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public bool OrderPaid { get; set; }
+
+        public Collection<OrderLine> OrderLines { get; set; }
+    }
+}
---
+++ BoxCar.Ordering/Entities/OrderLine.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BoxCar.Ordering.Entities
+{
+    public class OrderLine
+    {
+        public Guid VehicleId { get; set; }
+
+        public Guid EngineId { get; set; }
+
+        public Guid ChassisId { get; set; }
+
+        public Guid OptionPackId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int UnitPrice { get; set; }
+
+        public Order Order { get; set; }
+    }
+}
