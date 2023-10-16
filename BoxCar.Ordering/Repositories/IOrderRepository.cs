85e9cfceda79199fa1d048bf9927af9a050d5da3
Emem Isaac <me@ememisaac.com>
2023-10-16 15:41:02 +0100

    Adds order data reading and writing

---
+++ BoxCar.Ordering/Repositories/IOrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using BoxCar.Ordering.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BoxCar.Ordering.Repositories
+{
+    public interface IOrderRepository
+    {
+        Task<List<Order>> GetOrdersForUser(Guid userId);
+        Task AddOrder(Order order);
+        Task<Order> GetOrderById(Guid orderId);
+        Task UpdateOrderPaymentStatus(Guid orderId, bool paid);
+
+    }
+}
