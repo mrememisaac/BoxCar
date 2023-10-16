236cd8f4b276056588bfacd36e70cf509cef9bea
Emem Isaac <me@ememisaac.com>
2023-10-16 15:38:09 +0100

    Persistence for order info

---
+++ BoxCar.Ordering/DbContexts/OrderDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BoxCar.Ordering.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BoxCar.Ordering.DbContexts
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        
+    }
+}
