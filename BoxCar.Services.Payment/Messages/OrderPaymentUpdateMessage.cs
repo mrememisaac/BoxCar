a8d1800a8b59bf3e3a60c89fa456d5f0f69b4bc1
Emem Isaac <me@ememisaac.com>
2023-10-16 20:29:44 +0100

    Adds payment request and update messages

---
+++ BoxCar.Services.Payment/Messages/OrderPaymentUpdateMessage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using BoxCar.Integration.Messages;
+
+namespace BoxCar.Services.Payment.Messages
+{
+    public class OrderPaymentUpdateMessage: IntegrationBaseMessage
+    {
+        public Guid OrderId { get; set; }
+        public bool PaymentSuccess { get; set; }
+    }
+}
