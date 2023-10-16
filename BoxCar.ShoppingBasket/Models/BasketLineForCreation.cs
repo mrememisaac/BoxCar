2e272309da59074f5ef92e355d100b24ba6ffcf8
Emem Isaac <me@ememisaac.com>
2023-10-16 14:37:41 +0100

    Gather full info about the line item

--- BoxCar.ShoppingBasket/Models/BasketLineForCreation.cs
+++ BoxCar.ShoppingBasket/Models/BasketLineForCreation.cs
@@ -9,6 +9,15 @@ namespace BoxCar.ShoppingBasket.Models
         public Guid VehicleId { get; set; }
 
         [Required]
+        public Guid EngineId { get; set; }
+
+        [Required]
+        public Guid ChassisId { get; set; }
+
+        [Required]
+        public Guid OptionPackId { get; set; }
+
+        [Required]
         public int Price { get; set; }
 
         [Required]
