f565a98a494201b38d0eed1539780cc0a5ca455f
Emem Isaac <me@ememisaac.com>
2023-10-16 20:30:12 +0100

    Adds payment model

---
+++ BoxCar.Services.Payment/Models/PaymentInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace BoxCar.Services.Payment.Models
+{
+    public class PaymentInfo
+    {
+        public int Total { get; set; }
+        public string CardNumber { get; set; }
+        public string CardName { get; set; }
+        public string CardExpiration { get; set; }
+    }
+}
