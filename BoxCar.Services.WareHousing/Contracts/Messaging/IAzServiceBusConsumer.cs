416e07d78a903526c5357ef83c0f02c7982acb14
Emem Isaac <me@ememisaac.com>
2023-10-17 20:51:53 +0100

    Adds service bus listener contracts

---
+++ BoxCar.Services.WareHousing/Contracts/Messaging/IAzServiceBusConsumer.cs
@@ -0,0 +1,8 @@
+﻿namespace BoxCar.Services.WareHousing.Contracts.Messaging
+{
+    public interface IAzServiceBusConsumer
+    {
+        void Start();
+        void Stop();
+    }
+}
---
+++ BoxCar.Services.WareHousing/Contracts/Messaging/IVehicleAzServiceBusConsumer.cs
@@ -0,0 +1,7 @@
+﻿namespace BoxCar.Services.WareHousing.Contracts.Messaging
+{
+    public interface IVehicleAzServiceBusConsumer : IAzServiceBusConsumer
+    {
+
+    }
+}
