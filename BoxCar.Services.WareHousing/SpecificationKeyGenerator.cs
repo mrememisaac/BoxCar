ca286da00fb7bc15c172752ea471019e34307cd0
Emem Isaac <me@ememisaac.com>
2023-10-17 21:47:44 +0100

    A standard way to generate vehicle specification to facilitate vehicle queries

---
+++ BoxCar.Services.WareHousing/SpecificationKeyGenerator.cs
@@ -0,0 +1,18 @@
+﻿namespace BoxCar.Services.WareHousing
+{
+    public static class SpecificationKeyGenerator
+    {
+        /// <summary>
+        /// Standard way to generate vehicle specification key for quickly confirming if we have an exact match
+        /// </summary>
+        /// <param name="vehicleId"></param>
+        /// <param name="chassisId"></param>
+        /// <param name="engineId"></param>
+        /// <param name="optionPackId"></param>
+        /// <returns>string in the format $"VehicleId-{vehicleId}-ChassisId-{chassisId}-EngineId-{engineId}-OptionPack-{optionPackId}";</returns>
+        public static string GenerateSpecificationKey(Guid vehicleId, Guid chassisId, Guid engineId, Guid optionPackId)
+        {
+            return $"VehicleId-{vehicleId}-ChassisId-{chassisId}-EngineId-{engineId}-OptionPack-{optionPackId}";
+        }
+    }
+}
