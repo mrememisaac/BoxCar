2cd99c0e2eacb65839b187301a04c26f686ad698
Emem Isaac <me@ememisaac.com>
2023-10-17 20:04:01 +0100

    Adds message listener implementations

---
+++ BoxCar.Catalogue.Core/Messaging/ChassisAzServiceBusConsumer.cs
@@ -0,0 +1,49 @@
+﻿using BoxCar.Integration.MessageBus;
+using BoxCar.Catalogue.Messages;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using BoxCar.Catalogue.Core.Contracts.Persistence;
+using BoxCar.Catalogue.Core.Contracts.Messaging;
+using Microsoft.Extensions.Logging;
+
+namespace BoxCar.Catalogue.Messaging
+{
+    public class ChassisAddedEventConsumer : AzServiceBusConsumerBase, IChassisAzServiceBusConsumer
+    {
+        private readonly string _chassisAddedEventTopic;
+        private readonly IReceiverClient _chassisAddedMessageReceiverClient;
+        private readonly IChassisRepository _chassisRepository;
+
+        public ChassisAddedEventConsumer(IConfiguration configuration, IMessageBus messageBus, 
+            IChassisRepository chassisRepository, 
+            ILoggerFactory loggerFactory)
+            : base(configuration, messageBus, loggerFactory)
+        {
+            _chassisAddedEventTopic = _configuration.GetValue<string>("ChassisAddedEventTopic");
+            _chassisAddedMessageReceiverClient = new SubscriptionClient(_connectionString, _chassisAddedEventTopic, _subscriptionName);
+            _chassisRepository = chassisRepository;
+        }
+
+        public void Start()
+        {
+            var messageHandlerOptions = new MessageHandlerOptions(OnServiceBusException) { MaxConcurrentCalls = 4 };
+            _chassisAddedMessageReceiverClient.RegisterMessageHandler(OnNewChassisMessageReceived, messageHandlerOptions);
+        }
+
+        private async Task OnNewChassisMessageReceived(Message message, CancellationToken token)
+        {
+            var body = Encoding.UTF8.GetString(message.Body);
+
+            var chassisAddedEvent = System.Text.Json.JsonSerializer.Deserialize<ChassisAddedEvent>(body);
+            var chassis = new Domain.Chassis(chassisAddedEvent.ChassisId, 
+                chassisAddedEvent.Name, chassisAddedEvent.Description, chassisAddedEvent.Price);
+            await _chassisRepository.CreateAsync(chassis, token);
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}
---
+++ BoxCar.Catalogue.Core/Messaging/EngineAddedEventConsumer.cs
@@ -0,0 +1,51 @@
+﻿using BoxCar.Integration.MessageBus;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using System.Text;
+using BoxCar.Catalogue.Messages;
+using Microsoft.Extensions.Configuration;
+using BoxCar.Catalogue.Core.Contracts.Persistence;
+using BoxCar.Catalogue.Core.Contracts.Messaging;
+using Microsoft.Extensions.Logging;
+
+namespace BoxCar.Catalogue.Messaging
+{
+
+    public class EngineAddedEventConsumer : AzServiceBusConsumerBase, IEngineAzServiceBusConsumer
+    {
+        private readonly string _engineAddedEventTopic;
+        private readonly IReceiverClient _engineAddedMessageReceiverClient;
+        private readonly IEngineRepository _engineRepository;
+
+        public EngineAddedEventConsumer(IConfiguration configuration, IMessageBus messageBus, 
+            IEngineRepository engineRepository,
+            ILoggerFactory loggerFactory)
+            : base(configuration, messageBus, loggerFactory)
+        {
+            _engineAddedEventTopic = _configuration.GetValue<string>("EngineAddedEventTopic");
+            _engineAddedMessageReceiverClient = new SubscriptionClient(_connectionString, _engineAddedEventTopic, _subscriptionName);
+            _engineRepository = engineRepository;
+        }
+
+        public void Start()
+        {
+            var messageHandlerOptions = new MessageHandlerOptions(OnServiceBusException) { MaxConcurrentCalls = 4 };
+            _engineAddedMessageReceiverClient.RegisterMessageHandler(OnNewEngineMessageReceived, messageHandlerOptions);
+        }
+
+        private async Task OnNewEngineMessageReceived(Message message, CancellationToken token)
+        {
+            var body = Encoding.UTF8.GetString(message.Body);
+
+            var engineAddedEvent = System.Text.Json.JsonSerializer.Deserialize<EngineAddedEvent>(body);
+
+            var engine = new Domain.Engine(engineAddedEvent.EngineId, engineAddedEvent.Name, engineAddedEvent.FuelType, 
+                engineAddedEvent.IgnitionMethod, engineAddedEvent.Strokes, engineAddedEvent.Price);
+            await _engineRepository.CreateAsync(engine, token);
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}
