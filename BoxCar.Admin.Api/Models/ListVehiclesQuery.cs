211146bf682474209f3561c6fe5efc3f74065ff0
Emem Isaac <me@ememisaac.com>
2023-10-19 00:47:04 +0100

    List query models

---
+++ BoxCar.Admin.Api/Models/ListVehiclesQuery.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BoxCar.Admin.Api.Models
+{
+    public class ListVehiclesQuery
+    {
+        [Required]
+        [MaxLength(100)]
+        [MinLength(0)]
+        public int PageSize = 50;
+
+        [Required]
+        [MinLength(0)]
+        public int PageNumber { get; set; }
+
+    }
+}
