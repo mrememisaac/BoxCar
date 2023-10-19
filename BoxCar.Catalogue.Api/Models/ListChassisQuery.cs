45b6463b219ae53520e67fb320aedca5f35c313c
Emem Isaac <me@ememisaac.com>
2023-10-19 01:02:41 +0100

    adds docker support

---
+++ BoxCar.Catalogue.Api/Models/ListChassisQuery.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BoxCar.Catalogue.Api.Models
+{
+    public class ListChassisQuery
+    {
+        [Required]
+        [MaxLength(100)]
+        [MinLength(0)]
+        public int PageSize = 50;
+
+        [Required]
+        [MinLength(0)]
+        public int PageNumber { get; set; }
+
+    }
+}
