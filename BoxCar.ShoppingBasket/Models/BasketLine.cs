fdd971e55084be339b3a2148b8b0014d8bf958c2
Emem Isaac <me@ememisaac.com>
2023-10-23 19:26:24 +0100

    Adds properties

--- BoxCar.ShoppingBasket/Models/BasketLine.cs
+++ BoxCar.ShoppingBasket/Models/BasketLine.cs
@@ -8,7 +8,6 @@ namespace BoxCar.ShoppingBasket.Models
         public Guid BasketId { get; set; }
         public Guid VehicleId { get; set; }
         public int Price { get; set; }
-        public int TicketAmount { get; set; }
-        public Vehicle Vehicle { get; set; }
+        public int Quantity { get; set; }
     }
 } 
--- BoxCar.ShoppingBasket/Models/Vehicle.cs
+++ BoxCar.ShoppingBasket/Models/Vehicle.cs
@@ -8,12 +8,16 @@ namespace BoxCar.ShoppingBasket.Models
 
         public string Name { get; set; } = null!;
 
-        public Chassis Chassis { get; set; }
+        public virtual Chassis? Chassis { get; set; }
 
-        public Engine Engine { get; set; }
+        public virtual Engine? Engine { get; set; }
 
-        public OptionPack OptionPack { get; set; }
+        public virtual OptionPack? OptionPack { get; set; }
 
         public int Price { get; set; }
+
+        public Guid ChassisId { get; set; }
+        public Guid EngineId { get; set; }
+        public Guid OptionPackId { get; set; }
     }
 }
