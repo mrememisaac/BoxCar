51d15f4f2ef703218679e04daeaeb93de7402ea1
Emem Isaac <me@ememisaac.com>
2023-10-23 19:43:08 +0100

    Adds custom exception

---
+++ BoxCar.Shared/Exceptions/ServiceBusAccessException.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxCar.Shared.Exceptions
+{
+    public class ServiceBusAccessException : Exception
+    {
+        public EventId EventId = new EventId(1, nameof(ServiceBusAccessException));
+
+        public HttpStatusCode StatusCode = HttpStatusCode.InternalServerError;
+        
+    }
+}
