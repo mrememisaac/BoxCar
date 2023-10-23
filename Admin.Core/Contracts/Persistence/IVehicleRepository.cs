9dc8e8b61a574021d054a0fdb4b091d96ae2d66d
Emem Isaac <me@ememisaac.com>
2023-10-23 18:43:19 +0100

    Get full vehicle graph

---
+++ Admin.Core/Contracts/Persistence/IVehicleRepository.cs
@@ -0,0 +1,9 @@
+﻿using BoxCar.Admin.Domain;
+
+namespace BoxCar.Admin.Core.Contracts.Persistence
+{
+    public interface IVehicleRepository : IAsyncRepository<Vehicle, Guid>
+    {
+        Task<Vehicle?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    }
+}
---
+++ BoxCar.Admin.Persistence/VehicleRepository.cs
@@ -0,0 +1,38 @@
+﻿using BoxCar.Admin.Core.Contracts.Persistence;
+using BoxCar.Admin.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BoxCar.Admin.Persistence
+{
+    public class VehicleRepository : BaseRepository<Vehicle, Guid>, IVehicleRepository
+    {
+        public VehicleRepository(BoxCarAdminDbContext dbContext) : base(dbContext)
+        { }
+        public async new Task<Vehicle?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            return await _dbContext.Vehicles
+                .Include(v => v.Chassis)
+                .Include(v => v.Engine)
+                .Include(v => v.OptionPack)
+                .ThenInclude(pack => pack.Options)
+                .FirstAsync(x => x.Id == id);
+        }
+
+        public new async Task<IReadOnlyList<Vehicle>> GetPagedAsync(int page, int pageSize, CancellationToken cancellationToken)
+        {
+            page = page < 0 ? 0 : page;
+            pageSize = pageSize < 0 ? 100 : pageSize;
+            pageSize = pageSize > 500 ? 500 : pageSize;
+            var result = await _dbContext.Vehicles
+                .Include(v => v.Chassis)
+                .Include(v => v.Chassis)
+                .Include(v => v.Engine)
+                .Include(v => v.OptionPack)
+                .ThenInclude(pack => pack.Options)
+                .Skip(page * pageSize).Take(pageSize)
+                .ToListAsync(cancellationToken);
+            return result.AsReadOnly();
+
+        }
+    }
+}
