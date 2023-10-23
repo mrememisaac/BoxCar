00fe599f1db31d3d949f2ff9b7d37a9ba91160e8
Emem Isaac <me@ememisaac.com>
2023-10-23 19:13:14 +0100

    return a placeholder value

--- BoxCar.Services.Notifications/Services/EmailGatewayService.cs
+++ BoxCar.Services.Notifications/Services/EmailGatewayService.cs
@@ -18,18 +18,7 @@ namespace BoxCar.Services.Notifications.Services
 
         public async Task<bool> SendEmail(EmailInfo emailInfo)
         {
-            var dataAsString = JsonSerializer.Serialize(emailInfo);
-            var content = new StringContent(dataAsString);
-            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-
-            var response = await client.PostAsync(configuration.GetValue<string>("ApiConfigs:ExternalPaymentGateway:Uri") + "/api/paymentapprover", content);
-
-            if (!response.IsSuccessStatusCode)
-                throw new ApplicationException($"Something went wrong calling the API: {response.ReasonPhrase}");
-
-            var responseString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-
-            return JsonSerializer.Deserialize<bool>(responseString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            return true;
 
         }
     }
